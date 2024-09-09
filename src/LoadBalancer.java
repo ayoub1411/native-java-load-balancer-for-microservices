60673189e2d2a02faa27095ed1bd45426eb3c363
ayoub1411 <ayoubhass077@gmail.com>
2024-09-09 20:52:36 +0100

    Final Commit

--- src/LoadBalancer.java
+++ src/LoadBalancer.java
@@ -5,6 +5,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class LoadBalancer extends ServerSocket {
+	
 	ExecutorService executorService = Executors.newFixedThreadPool(10);
 	
 	
